670357559d88e9f2d79a0aae7642e46c24b2d309
hjx <31310679+githubhjx@users.noreply.github.com>
2018-06-21 09:45:40 +0800

    Create ReadTFrecords.py

---
+++ ReadTFrecords.py
@@ -0,0 +1,29 @@
+from skimage import io, transform
+import glob
+import os
+import numpy as np
+import tensorflow as tf
+
+path = '../train.tfrecords'
+
+
+def read_and_decode(filename):
+    filename_queue = tf.train.string_input_producer([filename])
+
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+                                       features={
+                                           'label': tf.FixedLenFeature([], tf.int64),
+                                           'img_raw' : tf.FixedLenFeature([], tf.string),
+                                       })
+
+    img = tf.decode_raw(features['img_raw'], tf.uint8)
+    img = tf.reshape(img, [224, 224, 3])
+    img = tf.cast(img, tf.float32) * (1. / 255) - 0.5
+    label = tf.cast(features['label'], tf.int32)
+
+    return img, label
+
+
+img, label = read_and_decode(path)
